c0acd3e6dd9ae9c4b435a72f87b81d45c07567d9
MichPisani <michelinapisani@gmail.com>
2025-04-30 11:55:06 +0000

    navbar y home hecho

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,16 +3,19 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar bg-dark">
+			<div className="container mb-2">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<img src="src/assets/img/star wars.png" style={{maxHeight:"5vh"}}/>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
 					</Link>
 				</div>
+			</div>
+			<div className="laser-border">
+
 			</div>
 		</nav>
 	);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,37 @@
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 
   const {store, dispatch} =useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container mt-5">
+			<div className="row justify-content-center">
+				<div className="col-md-3 d-flex flex-column align-items-center justify-content-around item-box">
+					<img src="src/assets/img/Planets.png" className="img-fluid mb-2 custom-img"/>
+					<Link to="/" className=" btn laser-button">Planets</Link>
+				</div>
+				<div className="col-md-3 d-flex flex-column align-items-center justify-content-around item-box">
+					<img src="src/assets/img/Character.png" className="img-fluid mb-2 custom-img"/>
+					<Link to="/" className=" btn laser-button">Characters</Link>
+				</div>
+
+			</div>
+			<div className="row justify-content-center">
+				<div className="col-md-3 d-flex flex-column align-items-center justify-content-around item-box">
+					<img src="src/assets/img/Vehicle.png" className="img-fluid mb-2 custom-img"/>
+					<Link to="/" className=" btn laser-button">Vehicles</Link>
+				</div>
+				<div className="col-md-3 d-flex flex-column align-items-center justify-content-around item-box">
+					<img src="src/assets/img/Species.png" className="img-fluid custom-img"/>
+					<Link to="/" className=" btn laser-button">Species</Link>
+				</div>
+
+			</div>
+
+
 		</div>
 	);
 }; 
